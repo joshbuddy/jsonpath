108936192042da430204cef17dd20a6920a70de8
Mark Morga <mmorga@rackspace.com>
2016-08-10 15:56:49 -0500

    Added support for dot notation for attribute names containing a colon

--- lib/jsonpath.rb
+++ lib/jsonpath.rb
@@ -19,7 +19,7 @@ class JsonPath
         @path << token
       elsif token = scanner.scan(/@/)
         @path << token
-      elsif token = scanner.scan(/[a-zA-Z0-9_-]+/)
+      elsif token = scanner.scan(/[a-zA-Z0-9:_-]+/)
         @path << "['#{token}']"
       elsif token = scanner.scan(/'(.*?)'/)
         @path << "[#{token}]"
