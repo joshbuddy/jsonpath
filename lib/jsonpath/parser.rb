f91f82fcfd88dee64cbb9abd37ba596e328d669e
Gergely Brautigam <skarlso777@gmail.com>
2019-01-20 23:04:06 +0100

    Fix proper handling of nested groups (#106)
    * WIP
    
    * Closes #105
    
    * Not working yet.
    
    * One last test to fix.
    
    * Works!
    
    * Added some documentation around the logic

--- lib/jsonpath/parser.rb
+++ lib/jsonpath/parser.rb
@@ -8,22 +8,46 @@ class JsonPath
   class Parser
     def initialize(node)
       @_current_node = node
+      @_expr_map = {}
     end
 
+    # parse will parse an expression in the following way.
+    # Split the expression up into an array of legs for && and || operators.
+    # Parse this array into a map for which the keys are the parsed legs
+    # of the split. This map is then used to replace the expression with their
+    # corresponding boolean or numeric value. This might look something like this:
+    # ((false || false) && (false || true))
+    # Once this string is assembled... we proceed to evaluate from left to right.
+    # The above string is broken down like this:
+    # (false && (false || true))
+    # (false && true)
+    # false
     def parse(exp)
       exps = exp.split(/(&&)|(\|\|)/)
-      ret = parse_exp(exps.shift)
-      exps.each_with_index do |item, index|
-        case item
-        when '&&'
-          ret &&= parse_exp(exps[index + 1])
-        when '||'
-          ret ||= parse_exp(exps[index + 1])
-        end
+      construct_expression_map(exps)
+      @_expr_map.each {|k, v| exp.sub!(k, "#{v}")}
+      raise ArgumentError, "unmatched parenthesis in expression: #{exp}" unless check_parenthesis_count(exp)
+      while (exp.include?("("))
+        exp = parse_parentheses(exp)
       end
-      ret
+      bool_or_exp(exp)
     end
 
+    # Construct a map for which the keys are the expressions
+    # and the values are the corresponding parsed results.
+    # Exp.:
+    # {"(@['author'] =~ /herman|lukyanenko/i)"=>0}
+    # {"@['isTrue']"=>true}
+    def construct_expression_map(exps)
+      exps.each_with_index do |item, index|
+        next if item == '&&' || item == '||'
+        item = item.strip.gsub(/\)*$/, '').gsub(/^\(*/, '')
+        @_expr_map[item] = parse_exp(item)
+      end
+    end
+
+    # using a scanner break down the individual expressions and determine if
+    # there is a match in the JSON for it or not.
     def parse_exp(exp)
       exp = exp.sub(/@/, '').gsub(/^\(/, '').gsub(/\)$/, '').tr('"', '\'').strip
       scanner = StringScanner.new(exp)
@@ -78,6 +102,87 @@ class JsonPath
       return hash.fetch(keys.first) if keys.size == 1
       prev = keys.shift
       dig(keys, hash.fetch(prev))
+    end
+
+    # This will break down a parenthesis from the left to the right
+    # and replace the given expression with it's returned value.
+    # It does this in order to make it easy to eliminate groups
+    # one-by-one.
+    def parse_parentheses(str)
+      opening_index = 0
+      closing_index = 0
+
+      (0..str.length-1).step(1) do |i|
+        if str[i] == '('
+          opening_index = i
+        end
+        if str[i] == ')'
+          closing_index = i
+          break
+        end
+      end
+
+      to_parse = str[opening_index+1..closing_index-1]
+
+      # handle cases like (true && true || false && true) in
+      # one giant parenthesis.
+      top = to_parse.split(/(&&)|(\|\|)/)
+      top = top.map{|t| t.strip}
+      res = bool_or_exp(top.shift)
+      top.each_with_index do |item, index|
+        case item
+        when '&&'
+          res &&= top[index + 1]
+        when '||'
+          res ||= top[index + 1]
+        end
+      end
+
+      # if we are at the last item, the opening index will be 0
+      # and the closing index will be the last index. To avoid
+      # off-by-one errors we simply return the result at that point.
+      if closing_index+1 >= str.length && opening_index == 0
+        return "#{res}"
+      else
+        return "#{str[0..opening_index-1]}#{res}#{str[closing_index+1..str.length]}"
+      end
+    end
+
+    # This is convoluted and I should probably refactor it somehow.
+    # The map that is created will contain strings since essentially I'm
+    # constructing a string like `true || true && false`.
+    # With eval the need for this would disappear but never the less, here
+    # it is. The fact is that the results can be either boolean, or a number
+    # in case there is only indexing happening like give me the 3rd item... or
+    # it also can be nil in case of regexes or things that aren't found.
+    # Hence, I have to be clever here to see what kind of variable I need to
+    # provide back.
+    def bool_or_exp(b)
+      if "#{b}" == 'true'
+        return true
+      elsif "#{b}" == 'false'
+        return false
+      elsif "#{b}" == ""
+        return nil
+      end
+      b = Float(b) rescue b
+      b
+    end
+
+    # this simply makes sure that we aren't getting into the whole
+    # parenthesis parsing business without knowing that every parenthesis
+    # has its pair.
+    def check_parenthesis_count(exp)
+      return true unless exp.include?("(")
+      depth = 0
+      exp.chars.each do |c|
+        if c == '('
+          depth += 1
+        elsif c == ')'
+          depth -= 1
+        end
+      end
+      depth == 0
     end
   end
 end
--- test/test_jsonpath.rb
+++ test/test_jsonpath.rb
@@ -717,6 +717,17 @@ class TestJsonpath < MiniTest::Unit::TestCase
     assert_equal [true], JsonPath.on(json, broken_path)
   end
 
+  def test_complex_nested_grouping
+    path = "$..book[?((@['author'] == 'Evelyn Waugh' || @['author'] == 'Herman Melville') && (@['price'] == 33 || @['price'] == 9))]"
+    assert_equal [@object['store']['book'][2]], JsonPath.new(path).on(@object)
+  end
+
+  def test_complex_nested_grouping_unmatched_parent
+    path = "$..book[?((@['author'] == 'Evelyn Waugh' || @['author'] == 'Herman Melville' && (@['price'] == 33 || @['price'] == 9))]"
+    err = assert_raises(ArgumentError, "should have raised an exception") { JsonPath.new(path).on(@object)}
+    assert_match(/unmatched parenthesis in expression: \(\(false \|\| false && \(false \|\| true\)\)/, err.message)
+  end
+
   def test_delete_more_items
     a = { 'itemList' =>
       [{ 'alfa' => 'beta1' },
